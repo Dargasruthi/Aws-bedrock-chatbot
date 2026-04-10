30b1e7130ddb9273102e8a5fce3effb6ad0ef71a
Darga sruthi <dargasruthi123@gmail.com>
2026-04-10 21:24:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,32 +8,27 @@ client = boto3.client(
     region_name="us-east-1"
 )
 
-# IMPORTANT: replace this with your real Knowledge Base ID
-KNOWLEDGE_BASE_ID = "GHL8UMPSQK"
-
-# Safe working model (NO PAYMENT ISSUE)
-MODEL_ARN = "amazon.titan-text-express-v1"
+KNOWLEDGE_BASE_ID = "7RIOJANXRR"
 
 user_input = st.text_input("Ask your question:")
 
 if st.button("Get Answer"):
 
     if user_input:
-
         response = client.retrieve_and_generate(
-    input={
-        "text": user_input
-    },
-    retrieveAndGenerateConfiguration={
-        "type": "KNOWLEDGE_BASE",
-        "knowledgeBaseConfiguration": {
-            "knowledgeBaseId": KNOWLEDGE_BASE_ID,
-            "modelArn": "anthropic.claude-3-haiku-20240307-v1:0"
-        }
-    }
-)
+            input={"text": user_input},
+            retrieveAndGenerateConfiguration={
+                "type": "KNOWLEDGE_BASE",
+                "knowledgeBaseConfiguration": {
+                    "knowledgeBaseId": KNOWLEDGE_BASE_ID,
+                    "modelArn": "anthropic.claude-3-haiku-20240307-v1:0"
+                }
+            }
+        )
 
-        answer = response["output"]["text"]
+        st.write(response["output"]["text"])
+    else:
+        st.warning("Please enter a question")
 
         st.subheader("Answer:")
         st.write(answer)
